3607f4ac16e62f94b2965b27213579e8eee793fd
harmeet2094 <harmeet2094@gmail.com>
2020-11-05 14:33:46 +0530

    routing for product details page

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.getAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
-    const price = req.body.price;    
+    const price = req.body.price;     
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
