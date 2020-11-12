d5d6731a7adb1dcd72445f6c26cbb0c933e4f20f
harmeet2094 <harmeet2094@gmail.com>
2020-11-12 17:17:08 +0530

    delete route and cart logic complete

--- models/product.js
+++ models/product.js
@@ -51,7 +51,7 @@ module.exports = class Product {
 
     static deleteById(id) {
         getProductFromFile(products => {
-            const product = products.find(prod => prod.id === id)
+            const product = products.find(prod => prod.id === id);
             const updatedProducts = products.filter(prod => prod.id !== id);
             fs.writeFile(p, JSON.stringify(updatedProducts), err => {
                 if(!err){
